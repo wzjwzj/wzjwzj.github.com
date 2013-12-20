77578579f20f7b6ebc7ae4baa0ee2c7e568bfd37
zhenjie wang <wzjwzj@mail.ustc.edu.cn>
2013-12-20 02:12:29 +0100

    Add new content

--- _posts/copy.sh
+++ _posts/copy.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/sh
-find /cygdrive/c/Users/Administrator/Dropbox/应用/FarBox/wzjwzj.farbox.com/ | egrep ".md$" | gawk '{print "cp",$1,"./"} END {print "rm  comment_js.md"}'
+find /cygdrive/c/Users/Administrator/Dropbox/应用/FarBox/wzjwzj.farbox.com/ | egrep ".md$" | gawk '{print "cp",$1,"./"} END {print "rm  comment_js.md isaid.md"}'
